f4ded0748144bfe0c40ba5976dfad8a3775a5ff5
Alexander Martyshin <alex26071997@gmail.com>
2025-02-10 18:42:01 +0300

    changed signature

--- src/main/java/org/example/finalproject/dto/MutatedList.java
+++
@@ -1,8 +0,0 @@
-package org.example.finalproject.dto;
-
-public record MutatedList<L, B>(L list, B isChanged) {
-    public  MutatedList(L list, B isChanged)  {
-        this.list = list;
-        this.isChanged = isChanged;
-    }
-}
