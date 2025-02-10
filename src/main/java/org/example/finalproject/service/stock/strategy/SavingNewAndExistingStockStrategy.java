22812dd323edbadc3902e20a1a5d395c85bff17e
Alexander Martyshin <alex26071997@gmail.com>
2025-02-10 18:47:12 +0300

    add interface

--- src/main/java/org/example/finalproject/service/stock/strategy/SavingNewAndExistingStockStrategy.java
+++ src/main/java/org/example/finalproject/service/stock/strategy/SavingNewAndExistingStockStrategy.java
@@ -1,12 +1,7 @@
 package org.example.finalproject.service.stock.strategy;
 
-import lombok.RequiredArgsConstructor;
 import org.example.finalproject.dto.stock.StockSaveRequestDto;
 import org.example.finalproject.model.entity.Stock;
-import org.example.finalproject.service.stock.AddToStockService;
-import org.example.finalproject.service.stock.GetStockService;
-import org.example.finalproject.service.stock.SerializableSaveService;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
