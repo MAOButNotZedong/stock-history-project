d781e2a85c7ad2f40712bfa012c5afbe3f8033f2
Alexander Martyshin <alex26071997@gmail.com>
2025-02-10 18:43:25 +0300

    add query for get seq last value

--- src/main/java/org/example/finalproject/dao/StockRepository.java
+++ src/main/java/org/example/finalproject/dao/StockRepository.java
@@ -9,9 +9,16 @@ import java.util.List;
 
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 
+    String QUERY_GET_LAST_SEQ_VALUE = "SELECT last_value FROM " + Stock.STOCKS_ID_SEQ;
+
     @Query("SELECT s FROM Stock s WHERE s.ticker.tickerSymbol = ?1  AND (s.date BETWEEN ?2 AND ?3) ORDER BY s.date ASC")
     List<Stock> findStockDates (String ticker, LocalDate startDate, LocalDate endDate);
 
     @Query("SELECT DISTINCT s FROM Stock s JOIN FETCH s.userStocks us WHERE s.ticker.tickerSymbol = ?1  AND us.user.id= ?2 ORDER BY s.date ASC")
     List<Stock> getStockHistory(String ticker, int userId);
+
+    @Query(value = QUERY_GET_LAST_SEQ_VALUE, nativeQuery = true)
+    int getLastSequenceValue();
 }
+
+
